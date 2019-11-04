385508c8f11e63b9133d00c43b873e6bc4f1b5a2
Carlos Celis <lscarloscelis@gmail.com>
2019-11-03 22:25:17 -0500

    Probando

---
+++ Api/Controllers/FotosController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FotosController : ControllerBase
+    {
+        
+    }
+}
